6f8061f0cedd352b210feb25f86f794175e7b4a2
linhtiendev <lelinhtien0707@gmail.com>
2023-08-09 21:12:53 +0700

    create movie-grid (input search movie)

--- src/components/movie-grid/MovieGrid.jsx
+++ src/components/movie-grid/MovieGrid.jsx
@@ -5,7 +5,7 @@ import './movie-grid.scss';
 
 import MovieCard from '../movie-card/MovieCard';
 import Button, { OutlineButton } from '../button/Button';
-// import Input from '../input/Input'
+import Input from '../input/Input'
 
 import tmdbApi, { category, movieType, tvType } from '../../api/tmdbApi';
 
@@ -42,7 +42,6 @@ const MovieGrid = props => {
         getList();
     }, [props.category, keyword]);
 
-
     const loadMore = async () => {
         let response = null;
         if (keyword === undefined) {
@@ -66,22 +65,67 @@ const MovieGrid = props => {
         setItems([...items, ...response.results]);
         setPage(page + 1);
     }
-  return (
-    <>
-        <div className="movie-grid">
+
+    return (
+        <>
+            <div className="section mb-3">
+                <MovieSearch category={props.category} keyword={keyword}/>
+            </div>
+            <div className="movie-grid">
+                {
+                    items.map((item, i) => <MovieCard category={props.category} item={item} key={i}/>)
+                }
+            </div>
             {
-                items.map((item, i) => <MovieCard category={props.category} item={item} key={i}/>)
+                page < totalPage ? (
+                    <div className="movie-grid__loadmore">
+                        <OutlineButton className="small" onClick={loadMore}>Load more</OutlineButton>
+                    </div>
+                ) : null
             }
-        </div>
-        {
-            page < totalPage ? (
-                <div className="movie-grid__loadmore">
-                    <OutlineButton className="small" onClick={loadMore}>Load more</OutlineButton>
-                </div>
-            ) : null
+        </>
+    );
+}
+
+const MovieSearch = props => {
+
+    const history = useHistory();
+
+    const [keyword, setKeyword] = useState(props.keyword ? props.keyword : '');
+
+    const goToSearch = useCallback(
+        () => {
+            if (keyword.trim().length > 0) {
+                history.push(`/${category[props.category]}/search/${keyword}`);
+            }
+        },
+        [keyword, props.category, history]
+    );
+
+    useEffect(() => {
+        const enterEvent = (e) => {
+            e.preventDefault();
+            if (e.keyCode === 13) {
+                goToSearch();
+            }
         }
-    </>
-  );
+        document.addEventListener('keyup', enterEvent);
+        return () => {
+            document.removeEventListener('keyup', enterEvent);
+        };
+    }, [keyword, goToSearch]);
+
+    return (
+        <div className="movie-search">
+            <Input
+                type="text"
+                placeholder="Enter keyword"
+                value={keyword}
+                onChange={(e) => setKeyword(e.target.value)}
+            />
+            <Button className="small" onClick={goToSearch}>Search</Button>
+        </div>
+    )
 }
 
 export default MovieGrid;
